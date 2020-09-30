6eb3064e7522ee40a621c9cd15f3097d50091f66
DesktopECHO <33142753+DesktopECHO@users.noreply.github.com>
2020-09-30 00:05:14 -0300

    Update PH4WSL1.cmd

--- PH4WSL1.cmd
+++ PH4WSL1.cmd
@@ -1,47 +1,61 @@
 @ECHO OFF
 POWERSHELL.EXE -command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 CLS
-ECHO.WSL1 Install Helper for Pi-hole 5.0  //  Daniel Milisic
-ECHO.-------------------------------------------------------------------
-ECHO.Enter the IPv4 and subnet mask in CIDR format that Pi-hole will use
-ECHO.Example: 192.168.3.99/24
+ECHO.Pi-hole 5.x for WSL
+ECHO.------------------------------------------------------------------
+ECHO.Pi-hole listener IP and subnet in CIDR format, ie: 192.168.3.99/24
 SET /p IPSM=Respone: 
+ECHO.
+ECHO.Port for Pi-hole. Port 80 is good if you don't have a webserver, or hit enter for default [8880]: 
+SET PORT=8880& SET /p PORT=Respone: 
+ECHO.
+SET DISTRO=Pi-hole
+IF NOT EXIST %TEMP%\Ubuntu.zip POWERSHELL.EXE -Command "Start-BitsTransfer -source https://aka.ms/wslubuntu2004 -destination %TEMP%\Ubuntu.zip"
+POWERSHELL.EXE -Command "Expand-Archive -Path %TEMP%\Ubuntu.zip -DestinationPath %TEMP% -force
+MKDIR "%PROGRAMFILES%\Pi-hole"
+ECHO.Fetching LxRunOffline...
+POWERSHELL.EXE -Command "wget https://github.com/DesktopECHO/xWSL/raw/master/LxRunOffline.exe -UseBasicParsing -OutFile '%PROGRAMFILES%\Pi-hole\LxRunOffline.exe'"
+SET GO="%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c 
+ECHO.
+ECHO.Installing distro...
+START /WAIT /MIN "Install Distro" "%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" "i" "-n" "Pi-hole" "-f" "%TEMP%\install.tar.gz" -d "%PROGRAMFILES%\Pi-hole" 
+ECHO. 
+ECHO.Configuring distro, this can take a few minutes...
+ECHO. 
+SET GO="%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c 
+%GO% "rm -rf /etc/apt/apt.conf.d/20snapd.conf /etc/rc2.d/S01whoopsie /etc/init.d/console-setup.sh ; rm -rf  /var/lib/dpkg/info/udev.postinst"
+%GO% "apt-get -qq update ; apt-get -qq --purge remove openssh-server openssh-sftp-server apport snapd open-iscsi plymouth open-vm-tools mdadm rsyslog ufw irqbalance lvm2 multipath-tools cloud-init cryptsetup cryptsetup-bin cryptsetup-run dbus-user-session dmsetup eject friendly-recovery init libcryptsetup12 libdevmapper1.02.1 libnss-systemd libpam-systemd libparted2 netplan.io packagekit packagekit-tools parted policykit-1 software-properties-common systemd systemd-sysv systemd-timesyncd ubuntu-standard xfsprogs udev --autoremove --allow-remove-essential" > NUL
+%GO% "apt-get -qq install inetutils-syslogd dns-root-data dnsutils gamin idn2 libgamin0 lighttpd netcat php-cgi php-common php-intl php-sqlite3 php-xml php7.4-cgi php7.4-cli php7.4-common php7.4-intl php7.4-json php7.4-opcache php7.4-readline php7.4-sqlite3 php7.4-xml sqlite3 unzip dhcpcd5 nano cron" > NUL
+%GO% "mkdir /etc/pihole ; touch /etc/network/interfaces"
+%GO% "echo BLOCKING_ENABLED=true > /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_INTERFACE=eth0 >> /etc/pihole/setupVars.conf"
+%GO% "echo IPV4_ADDRESS=%IPSM% >> /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_DNS_1=8.8.8.8 >> /etc/pihole/setupVars.conf"
+%GO% "echo PIHOLE_DNS_2=8.8.4.4 >> /etc/pihole/setupVars.conf"
+%GO% "echo QUERY_LOGGING=true >> /etc/pihole/setupVars.conf"
+%GO% "echo INSTALL_WEB_SERVER=true >> /etc/pihole/setupVars.conf"
+%GO% "echo INSTALL_WEB_INTERFACE=true >> /etc/pihole/setupVars.conf"
+%GO% "echo LIGHTTPD_ENABLED=true >> /etc/pihole/setupVars.conf"
+%GO% "curl -L https://install.Pi-hole.net | bash /dev/stdin --unattended"
+%GO% "echo."
+%GO% "echo Web Interface Admin ; pihole -a -p"
+%GO% "sed -i 's/= 80/= %PORT%/g' /etc/lighttpd/lighttpd.conf"
+%GO% "touch /var/run/syslog.pid ; chmod 600 /var/run/syslog.pid"
 
-IF NOT EXIST %TEMP%\ubuntu-20.04-server-cloudimg-amd64-wsl.rootfs.tar.gz POWERSHELL.EXE -Command "wget https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64-wsl.rootfs.tar.gz -OutFile %TEMP%\ubuntu-20.04-server-cloudimg-amd64-wsl.rootfs.tar.gz"
-POWERSHELL.EXE -Command "wget https://github.com/DesktopECHO/xWSL/raw/master/LxRunOffline.exe -UseBasicParsing -OutFile %PROGRAMDATA%\LxRunOffline.exe"
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole Admin Page" dir=in action=allow protocol=TCP localport=%PORT% > NUL
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole DNS (TCP)" dir=in action=allow protocol=TCP localport=53 > NUL
+NetSH AdvFirewall Firewall add rule name="WSL Pi-hole DNS (UDP)" dir=in action=allow protocol=UDP localport=53 > NUL
 
-%PROGRAMDATA%\LxRunOffline.exe i -d %PROGRAMDATA%\Pi-hole -f %TEMP%\ubuntu-20.04-server-cloudimg-amd64-wsl.rootfs.tar.gz -n Pi-hole
-
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "add-apt-repository -y ppa:rafaeldtinoco/lp1871129"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "apt-get -y install libc6=2.31-0ubuntu8+lp1871129~1 libc-dev-bin=2.31-0ubuntu8+lp1871129~1 --allow-downgrades ; apt-mark hold libc6"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "rm -rf /etc/apt/apt.conf.d/20snapd.conf /etc/rc2.d/S01whoopsie /etc/init.d/console-setup.sh ; rm -rf  /var/lib/dpkg/info/udev.postinst"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "apt-get -y --purge remove openssh-server openssh-sftp-server apport snapd open-iscsi plymouth open-vm-tools mdadm rsyslog ufw irqbalance lvm2 multipath-tools cloud-init cryptsetup cryptsetup-bin cryptsetup-run dbus-user-session dmsetup eject friendly-recovery init libcryptsetup12 libdevmapper1.02.1 libnss-systemd libpam-systemd libparted2 netplan.io packagekit packagekit-tools parted policykit-1 software-properties-common systemd systemd-sysv systemd-timesyncd ubuntu-standard xfsprogs udev --autoremove --allow-remove-essential"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "apt-get -y install dns-root-data dnsutils gamin idn2 libgamin0 lighttpd netcat php-cgi php-common php-intl php-sqlite3 php-xml php7.4-cgi php7.4-cli php7.4-common php7.4-intl php7.4-json php7.4-opcache php7.4-readline php7.4-sqlite3 php7.4-xml sqlite3 unzip dhcpcd5 nano cron"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "mkdir /etc/pihole ; touch /etc/network/interfaces"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo BLOCKING_ENABLED=true > /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo PIHOLE_INTERFACE=eth0 >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo IPV4_ADDRESS=%IPSM% >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo PIHOLE_DNS_1=8.8.8.8 >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo PIHOLE_DNS_2=8.8.4.4 >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo QUERY_LOGGING=true >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo INSTALL_WEB_SERVER=true >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo INSTALL_WEB_INTERFACE=true >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo LIGHTTPD_ENABLED=true >> /etc/pihole/setupVars.conf"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "curl -L https://install.Pi-hole.net | bash /dev/stdin --unattended"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "echo Web Interface Admin ; pihole -a -p"
-%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "sed -i 's/= 80/= 10080/g'  /etc/lighttpd/lighttpd.conf"
-
-NetSH AdvFirewall Firewall add rule name="Pi-hole for WSL Admin" dir=in action=allow protocol=TCP localport=10080
-NetSH AdvFirewall Firewall add rule name="Pi-hole for DNS (TCP)" dir=in action=allow protocol=TCP localport=53
-NetSH AdvFirewall Firewall add rule name="Pi-hole for DNS (UDP)" dir=in action=allow protocol=UDP localport=53
-
-SCHTASKS /CREATE /RU %USERNAME% /RL HIGHEST /SC ONSTART /TN "Pi-hole for WSL Launcher" /TR %PROGRAMDATA%\Pi-hole.cmd /F 
-ECHO $task = Get-ScheduledTask "Pi-hole for WSL Launcher" ; $task.Settings.ExecutionTimeLimit = "PT0S" ; Set-ScheduledTask $task > %TEMP%\ExecTimeLimit.ps1
+SCHTASKS /CREATE /RU %USERNAME% /RL HIGHEST /SC ONSTART /TN "Pi-hole for WSL" /TR '"%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"' /F
+ECHO $task = Get-ScheduledTask "Pi-hole for WSL" ; $task.Settings.ExecutionTimeLimit = "PT0S" ; Set-ScheduledTask $task > %TEMP%\ExecTimeLimit.ps1
 POWERSHELL -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -COMMAND %TEMP%\ExecTimeLimit.ps1
 
-ECHO @IF [%1]==[-r] C:\ProgramData\LxRunOffline.exe r -n Pi-hole -c "pihole -r"                                                                > %PROGRAMDATA%\Pi-hole.cmd
-ECHO @%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "sed -i 's/= 80/= 10080/g'  /etc/lighttpd/lighttpd.conf"                                 >> %PROGRAMDATA%\Pi-hole.cmd
-ECHO @%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "for rc_service in /etc/rc2.d/S*; do [[ -e $rc_service ]] && $rc_service restart ; done" >> %PROGRAMDATA%\Pi-hole.cmd
-     @%PROGRAMDATA%\LxRunOffline.exe r -n Pi-hole -c "for rc_service in /etc/rc2.d/S*; do [[ -e $rc_service ]] && $rc_service start   ; done"
-     @START "First Run" http://%COMPUTERNAME%:10080/admin
-
+ECHO @"%PROGRAMFILES%\Pi-hole\LxRunOffline.exe" r -n Pi-hole -c "pihole -r" > "%PROGRAMFILES%\Pi-hole\Pi-hole_ResetReconfigure.cmd"
+ECHO @"%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" r -n Pi-hole -c "apt-get -qq remove dhcpcd5" > "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd" 
+ECHO @"%PROGRAMFILES%\Pi-Hole\LxRunOffline.exe" r -n Pi-hole -c "sed -i 's/= 80/= %PORT%/g'  /etc/lighttpd/lighttpd.conf" >> "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"
+ECHO @%GO% "for rc_service in /etc/rc2.d/S*; do [[ -e $rc_service ]] && $rc_service restart ; done" >> "%PROGRAMFILES%\Pi-hole\Pi-hole_RunTask.cmd"
+SCHTASKS /RUN /TN "Pi-hole for WSL"
+ECHO Wait for launcher window to close then
+PAUSE
+ECHO Pi-hole 5.x for WSL Install Complete!
+START http://%COMPUTERNAME%:%PORT%/admin
